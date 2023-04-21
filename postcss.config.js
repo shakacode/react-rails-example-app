d6428aea08205e3f30dec23ff59ffe3cec3b3cb5
Mostafa Ahangarha <ahangarha@riseup.net>
2023-04-21 23:18:42 +0330

    Update files and js packages to match Webpacker req

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-smart-import'),
-    require('postcss-cssnext'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
