6bbbcb6e30a9149a91327486354a8e45e53d817a
Mostafa Ahangarha <ahangarha@riseup.net>
2023-08-25 22:37:22 +0330

    Update babel config

--- babel.config.js
+++ babel.config.js
@@ -2,36 +2,22 @@
 module.exports = function(api) {
   const defaultConfigFunc = require('shakapacker/package/babel/preset.js')
   const resultConfig = defaultConfigFunc(api)
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
-
-  if (!validEnv.includes(currentEnv)) {
-    throw new Error(
-      'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' +
-        JSON.stringify(currentEnv) +
-        '.'
-    )
-  }
+  const isProductionEnv = api.env('production')
 
   const changesOnDefault = {
     presets: [
       [
         '@babel/preset-react',
         {
-          development: isDevelopmentEnv || isTestEnv,
+          development: !isProductionEnv,
           useBuiltIns: true
-        } 
+        }
       ]
     ].filter(Boolean),
     plugins: [
-      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types', 
-        { 
-          removeImport: true 
+      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types',
+        {
+          removeImport: true
         }
       ],
       process.env.WEBPACK_SERVE && 'react-refresh/babel'
